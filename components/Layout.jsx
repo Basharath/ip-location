62f2af05ed597d32eb7bbce73b1cbc793b553cf6
Basharath <basharath.in@gmail.com>
2021-09-06 19:50:06 +0530

    Setsup material ui v5

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Layout() {
+  return (
+    <>
+      <Head>
+        <title>IP location finder</title>
+      </Head>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import { Typography, Box } from '@mui/material';
+
 export default function Home() {
-  return <div>Welcome to IP location finder</div>;
+  return (
+    <Box mt={5} textAlign='center'>
+      <Typography variant='h4' component='h1' color='primary'>
+        Welcome to IP location finder
+      </Typography>
+    </Box>
+  );
 }
