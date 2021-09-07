741726ed20c1275cafc2b29f376c460f70e2d1c6
Basharath <basharath.in@gmail.com>
2021-09-07 22:36:40 +0530

    Updates home page and search page

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable camelcase */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-// import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from '@mui/material/CircularProgress';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
@@ -18,102 +18,155 @@ import ListItemText from '@mui/material/ListItemText';
 import Layout from './Layout';
 
 export default function Main({ data }) {
-  const [search, setSearch] = useState('');
-  const isMobile = useMediaQuery('(max-width: 540px)');
   const router = useRouter();
+  const q = router.query.ip;
+  const [search, setSearch] = useState(q || '');
+  const [isValidIp, setIsValidIp] = useState(true);
+  const isMobile = useMediaQuery('(max-width: 540px)');
+
+  useEffect(() => {
+    if (q) setSearch(q);
+  }, [q]);
 
   const {
     ip,
     country_flag,
-    // isp,
-    // city,
-    // state,
-    country,
+    isp,
+    city,
+    state_prov,
+    country_name,
+    country_capital,
+    country_code2,
     // capital,
-    // country_code,
     // currency,
-    // currency_symbol,
-    // latitute,
-    // longitude,
-    // timezone,
-    // timezone_name,
-    // timezone_gmt,
+    latitude,
+    longitude,
+    // time_zone,
+    zipcode,
     client,
   } = data;
 
-  const handleChange = ({ currentTarget: { value } }) => setSearch(value);
+  const handleChange = ({ currentTarget: { value } }) => {
+    const ipRegex = new RegExp(
+      '^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\\.(?!$)|$)){4}$'
+    );
+    const isValid = ipRegex.test(value);
+    setIsValidIp(isValid);
+    setSearch(value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    if (!isValidIp) return;
     router.push(`/search?ip=${search}`);
-    console.log('Submitted', search);
+    if (!search) router.push('/');
   };
+
   return (
     <Layout>
-      <Box
-        sx={{
-          border: !isMobile && '1px solid #c4c4c4',
-          p: isMobile ? 2 : 8,
-          py: 4,
-          borderRadius: 2,
-          textAlign: 'center',
-          mb: 2,
-        }}
-      >
-        <Paper
-          component='form'
-          sx={{
-            p: '2px 4px',
-            display: 'flex',
-            alignItems: 'center',
-            width: isMobile ? 300 : 400,
-            mb: 3,
-          }}
-          elevation={3}
-          onSubmit={handleSubmit}
-        >
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder='Enter an IP address'
-            inputProps={{
-              'aria-label': 'Enter an IP address',
-              maxLength: 15,
+      {!data ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Box
+            sx={{
+              border: !isMobile && '1px solid #c4c4c4',
+              p: isMobile ? 2 : 8,
+              py: 4,
+              borderRadius: 2,
+              textAlign: 'center',
+              mb: 2,
             }}
-            onChange={handleChange}
-          />
-          <IconButton type='submit' sx={{ p: '10px' }} aria-label='search'>
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography mr={2}>{client ? `Your IP: ${ip}` : ip}</Typography>
-          <Image
-            src={country_flag}
-            // src='https://ipgeolocation.io/static/flags/in_64.png'
-            alt={country}
-            width={60}
-            height={30}
-          />
-        </Box>
-      </Box>
-      <Box>
-        <Typography>IP address info</Typography>
-        <List>
-          <ListItem>
-            {/* <ListItemIcon>
+          >
+            <Paper
+              component='form'
+              sx={{
+                p: '2px 4px',
+                display: 'flex',
+                alignItems: 'center',
+                width: isMobile ? 300 : 400,
+                mb: 3,
+              }}
+              elevation={3}
+              onSubmit={handleSubmit}
+            >
+              <InputBase
+                sx={{
+                  ml: 1,
+                  flex: 1,
+                  border: !isValidIp && '1px solid red',
+                  borderRadius: 1,
+                  px: 1,
+                }}
+                placeholder='Enter an IP address'
+                inputProps={{
+                  'aria-label': 'Enter an IP address',
+                  maxLength: 15,
+                }}
+                fullWidth
+                onChange={handleChange}
+                error={!isValidIp}
+                value={search}
+                className='Mui-error'
+              />
+              <IconButton type='submit' sx={{ p: '10px' }} aria-label='search'>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Typography mr={2}>{client ? `Your IP: ${ip}` : ip}</Typography>
+              <Image
+                src={country_flag}
+                // src='https://ipgeolocation.io/static/flags/in_64.png'
+                alt={country_name}
+                width={60}
+                height={30}
+              />
+            </Box>
+          </Box>
+          <Box>
+            <Typography>IP address info</Typography>
+            <List dense>
+              <ListItem>
+                {/* <ListItemIcon>
                   <FolderIcon />
                 </ListItemIcon> */}
-            <ListItemText primary='Single-line item' />
-          </ListItem>
-        </List>
-      </Box>
+                <ListItemText primary={isp} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={city} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={state_prov} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={country_name} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={country_capital} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={latitude} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={longitude} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={country_code2} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={zipcode} />
+              </ListItem>
+            </List>
+          </Box>
+        </>
+      )}
     </Layout>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,51 +1,10 @@
 /* eslint-disable camelcase */
 // import useSWR from 'swr';
-import CircularProgress from '@mui/material/CircularProgress';
 import Main from '../components/Main';
 import { getGeoLocation } from '../src/httpService';
 
 export default function Home({ data }) {
-  // const { data, error } = useSWR('clientGeo', getClientGeo);
-
-  // if (error) return <div>failed to load</div>;
-
-  console.log('data', data);
-  const {
-    ip,
-    country_flag,
-    isp,
-    city,
-    state,
-    country,
-    capital,
-    country_code,
-    currency,
-    currency_symbol,
-    latitute,
-    longitude,
-    timezone,
-    timezone_gmt,
-  } = data || {};
-
-  const ipData = {
-    ip,
-    country_flag,
-    isp,
-    city,
-    state,
-    country,
-    capital,
-    country_code,
-    currency,
-    currency_symbol,
-    latitute,
-    longitude,
-    timezone,
-    timezone_gmt,
-    client: 'yes',
-  };
-
-  return !data ? <CircularProgress /> : <Main data={ipData} />;
+  return <Main data={{ ...data, client: true }} />;
 }
 
 export const getServerSideProps = async () => {
@@ -55,86 +14,3 @@ export const getServerSideProps = async () => {
     props: { data },
   };
 };
-
-// <Box
-//   sx={{
-//     mt: 9,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   }}
-// >
-//   <Typography sx={{ mb: 4 }} variant='h4' component='h1' color='primary'>
-//     IP location finder
-//   </Typography>
-
-//   {!data ? (
-//     <CircularProgress />
-//   ) : (
-//     <>
-//       <Box
-//         sx={{
-//           border: !isMobile && '1px solid #c4c4c4',
-//           p: isMobile ? 2 : 8,
-//           py: 4,
-//           borderRadius: 2,
-//           textAlign: 'center',
-//           mb: 2,
-//         }}
-//       >
-//         <Paper
-//           component='form'
-//           sx={{
-//             p: '2px 4px',
-//             display: 'flex',
-//             alignItems: 'center',
-//             width: isMobile ? 300 : 400,
-//             mb: 3,
-//           }}
-//           elevation={3}
-//           onSubmit={handleSubmit}
-//         >
-//           <InputBase
-//             sx={{ ml: 1, flex: 1 }}
-//             placeholder='Enter an IP address'
-//             inputProps={{
-//               'aria-label': 'Enter an IP address',
-//               maxLength: 15,
-//             }}
-//             onChange={handleChange}
-//           />
-//           <IconButton type='submit' sx={{ p: '10px' }} aria-label='search'>
-//             <SearchIcon />
-//           </IconButton>
-//         </Paper>
-//         <Box
-//           sx={{
-//             display: 'flex',
-//             alignItems: 'center',
-//             justifyContent: 'center',
-//           }}
-//         >
-//           <Typography mr={2}>Your IP: {data?.ip}</Typography>
-//           <Image
-//             src={data?.country_flag}
-//             // src='https://ipgeolocation.io/static/flags/in_64.png'
-//             alt={data?.country}
-//             width={60}
-//             height={30}
-//           />
-//         </Box>
-//       </Box>
-//       <Box>
-//         <Typography>IP address info</Typography>
-//         <List>
-//           <ListItem>
-//             {/* <ListItemIcon>
-//               <FolderIcon />
-//             </ListItemIcon> */}
-//             <ListItemText primary='Single-line item' />
-//           </ListItem>
-//         </List>
-//       </Box>
-//     </>
-//   )}
-// </Box>
