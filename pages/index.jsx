7c087e904d2936fbc816a00d629efe29193338b6
Basharath <basharath.in@gmail.com>
2021-09-08 18:14:01 +0530

    Adds an empty query on the index page to render on the server

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,8 +7,9 @@ export default function Home({ data }) {
   return <Main data={{ ...data, client: true }} />;
 }
 
-export const getServerSideProps = async () => {
-  const data = await getGeoLocation('');
+export const getServerSideProps = async ({ query }) => {
+  const { ip } = query;
+  const data = await getGeoLocation(ip || '');
 
   return {
     props: { data },
