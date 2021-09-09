bf1968f721d4192baf1aeb92b0107c8965ab762c
Basharath <basharath.in@gmail.com>
2021-09-09 22:33:22 +0530

    Adds nprogress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,32 @@
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import '../styles/globals.css';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  useEffect(() => {
+    const start = () => NProgress.start();
+    const end = () => NProgress.done();
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
 
   return (
     <CacheProvider value={emotionCache}>
